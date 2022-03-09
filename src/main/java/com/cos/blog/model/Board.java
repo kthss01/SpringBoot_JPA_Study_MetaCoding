3477815f229314566a70d6951e1fdee6d1a9d92f
kthss01 <kthss01@naver.com>
2022-03-09 20:02:49 +0900

    연관관계 주인, OneToMany

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,9 +42,17 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; // 조회수
 	
-	@ManyToOne // Many : Board, One : User => 한명은 여러 게시글을 쓸 수 있음
-	@JoinColumn(name="userId")
+	// FetchType Eager -> 바로 가져온다는거
+	// 기본 전략임 
+	@ManyToOne(fetch = FetchType.EAGER) // Many : Board, One : User => 한명은 여러 게시글을 쓸 수 있음
+	@JoinColumn(name = "userId")
 	private User user; // DB는 오브젝트를 저장할 수 없음 => FK , 자바는 오브젝트를 저장할 수 있다.
+
+	// mappedBy 연관관계의 주인이 아니다 (난 FK 아님) => DB에 컬럼 만들지 마세요
+	// mappedBy 뒤의 board는 Reply의 필드이름을 적는거임 board 필드
+	// 여긴 FetchType 기본이 LAZY라고 함
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER)
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
